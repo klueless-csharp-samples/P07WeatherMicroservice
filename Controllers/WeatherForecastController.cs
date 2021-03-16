8fd252c8f2b33e18e71a44ac500e8196b51e687a
klueless-io <david@ideasmen.com.au>
2021-03-17 10:13:11 +1100

    add screenshots

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,36 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using P07WeatherMicroservice.Services;
 
 namespace P07WeatherMicroservice.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly WeatherClient client;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, WeatherClient client)
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+      _logger = logger;
+      this.client = client;
     }
+
+    [HttpGet]
+    [Route("{city}")] // weather/Sydney,%22 AU
+    public async Task<WeatherForecast> Get(string city)
+    {
+      var forecast = await client.GetCurrentWeatherAsync(city);
+
+      return new WeatherForecast {
+        Summary = forecast.weather[0].description,
+        TemperatureC = (int) forecast.main.temp,
+        Date = DateTimeOffset.FromUnixTimeSeconds(forecast.dt).DateTime
+      };
+    }
+  }
 }
